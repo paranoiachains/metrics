1afdf99dd7b2b7dcd973ba29a679484c1bb78dd9
paranoiachains <bluntsside@gmail.com>
2025-04-03 00:31:50 +0300

    bugfix #7

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,6 +14,7 @@ import (
 
 func main() {
 	flags.ParseServerFlags()
+	os.Mkdir("../../tmp", 0666)
 	fmt.Printf("Restore?: %v\n", flags.Restore)
 	fmt.Printf("Path: %v\n", flags.FileStoragePath)
 	fmt.Printf("Interval: %v\n", flags.StoreInterval)
